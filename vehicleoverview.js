c041d398b63d147b20429819498147b55b6dab88
Levy <163390630+vLevyx@users.noreply.github.com>
2024-10-31 06:13:56 +0300

    Vehicle Function Updates

--- vehicleoverview.js
+++ vehicleoverview.js
@@ -1,21 +1,20 @@
 const vehicles = [
+    { name: "M1025 Light Armored Vehicle", price: 250000, ores: 18, honeycombs: 9999, photo: "m1025.png" },
+    { name: "M151A2 Off-Road", price: 25000, ores: 16, honeycombs: 9999, photo: "m151a2_cover.png" },
+    { name: "M151A2 Off-Road - Open Top", price: 25000, ores: 16, honeycombs: 9999, photo: "m151a2offroad-opentop.png" },
+    { name: "M998 Light Utility Vehicle", price: 150000, ores: 18, honeycombs: 9999, photo: "m998LUV.png" },
+    { name: "M998 Light Utility Vehicle - Canopy", price: 175000, ores: 18, honeycombs: 9999, photo: "m998LUVcanopy.png" },
+    { name: "M923A1 Fuel Truck", price: 1000000, ores: 53, honeycombs: 9999, photo: "m923a1_fuel.png" },
+    { name: "M923A1 Transport Truck", price: 800000, ores: 50, honeycombs: 9999, photo: "m923a1.png" },
+    { name: "M923A1 Transport Truck - Canopy", price: 1800000, ores: 83, honeycombs: 9999, photo: "m923a1_cover.png" },
+    { name: "Pickup Truck", price: 500000, ores: 18, honeycombs: 9999, photo: "pickuptruck.png" },
+    { name: "UAZ-452 Off-Road", price: 95000, ores: 28, honeycombs: 9999, photo: "uaz452offroad.png" },
     { name: "UAZ-469 Off-Road", price: 10000, ores: 13, honeycombs: 9999, photo: "uaz469_cover.png" },
     { name: "UAZ-469 Off-Road - Open Top", price: 10000, ores: 13, honeycombs: 9999, photo: "uaz469offroad-opentop.png" },
-    { name: "M151A2 Off-Road - Open Top", price: 25000, ores: 16, honeycombs: 9999, photo: "m151a2offroad-opentop.png" },
-    { name: "M151A2 Off-Road", price: 25000, ores: 16, honeycombs: 9999, photo: "m151a2_cover.png" },
-    { name: "UAZ-452 Off-Road", price: 95000, ores: 28, honeycombs: 9999, photo: "uaz452offroad.png" },
-    { name: "M923A1 Transport Truck", price: 800000, ores: 50, honeycombs: 9999, photo: "m923a1.png" },
-    { name: "M923A1 Fuel Truck", price: 1000000, ores: 53, honeycombs: 9999, photo: "m923a1_fuel.png" },
-    { name: "M923A1 Transport Truck - Canopy", price: 1800000, ores: 83, honeycombs: 9999, photo: "m923a1_cover.png" },
     { name: "Ural-4320 Fuel Truck", price: 2800000, ores: 83, honeycombs: 9999, photo: "ural4320_fuel.png" },
     { name: "Ural-4320 Transport Truck", price: 2800000, ores: 100, honeycombs: 9999, photo: "ural4320transporttruck.png" },
     { name: "Ural-4320 Transport Truck - Canopy", price: 4000000, ores: 116, honeycombs: 9999, photo: "ural4320_cover.png" },
-    { name: "S1203 Minibus", price: 185000, ores: 0, honeycombs: 9999, photo: "uaz452offroad.png" },
-    { name: "M998 Light Utility Vehicle", price: 150000, ores: 18, honeycombs: 9999, photo: "m998LUV.png" },
-    { name: "M998 Light Utility Vehicle - Canopy", price: 175000, ores: 18, honeycombs: 9999, photo: "m998LUVcanopy.png" },
-    { name: "M1025 Light Armored Vehicle", price: 250000, ores: 18, honeycombs: 9999, photo: "m1025.png" },
     { name: "VW Rolf", price: 800000, ores: 18, honeycombs: 9999, photo: "vwrolf.png" },
-    { name: "Pickup Truck", price: 500000, ores: 18, honeycombs: 9999, photo: "pickuptruck.png" },
     { name: "MI8-MT Transport Helicopter", price: 58000000, ores: 26, honeycombs: 9999, photo: "mi8-mt.png" },
     { name: "UH-1H Transport Helicopter", price: 47000000, ores: 26, honeycombs: 9999, photo: "uh-1h.png" }
 ];
@@ -25,11 +24,11 @@ const vehicles = [
 const discountRates = {
     neutral: 0,
     positive1: -5.5,
-    positive2: -10.5,
+    positive2: -5.5,
     positive3: -14.29,
     negative1: 5.5,
-    negative2: 10.5,
-    negative3: 14.29
+    negative2: 5.5,
+    negative3: 5.0
 };
 
 function renderVehicles() {
@@ -40,7 +39,7 @@ function renderVehicles() {
         const vehicleCard = document.createElement('div');
         vehicleCard.className = 'vehicle-card';
         vehicleCard.innerHTML = `
-            <img src="${vehicle.image}" alt="${vehicle.name}">
+            <img src="${vehicle.photo}" alt="${vehicle.name}">
             <div class="vehicle-info">
                 <h3>${vehicle.name}</h3>
                 <p>Price: $${vehicle.price.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
@@ -60,15 +59,16 @@ function updatePrices() {
     container.innerHTML = '';
 
     vehicles.forEach(vehicle => {
-        const adjustedPrice = vehicle.price * (1 - (discount / 100));
+        // Apply a discount for positive reputations (negative discount value) and an increase for negative (positive discount value)
+        const adjustedPrice = vehicle.price * (1 + (discount / 100));
         const vehicleCard = document.createElement('div');
         vehicleCard.className = 'vehicle-card';
         vehicleCard.innerHTML = `
             <img src="${vehicle.photo}" alt="${vehicle.name}">
             <div class="vehicle-info">
                 <h3>${vehicle.name}</h3>
-                <p>Price: $${adjustedPrice.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                <p>Ores: ${vehicle.ores}</p>
+                <p>Adjusted Price: $${Math.round(adjustedPrice).toLocaleString()}</p>
+                <p>Ores Capacity: ${vehicle.ores}</p>
                 <p>Honeycombs: ${vehicle.honeycombs}</p>
             </div>
         `;
